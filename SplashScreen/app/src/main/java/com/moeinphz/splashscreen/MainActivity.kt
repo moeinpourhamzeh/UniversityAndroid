81c91bffaf395cd3b3a358a5348af0ce2122663a
Moein <64212565+moeinpourhamzeh@users.noreply.github.com>
2023-03-11 12:59:23 +0330

    OnClickTextChange on TextView Implemented

--- SplashScreen/app/src/main/java/com/moeinphz/splashscreen/MainActivity.kt
+++ SplashScreen/app/src/main/java/com/moeinphz/splashscreen/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Looper
 import android.view.View
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -16,13 +17,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var button = findViewById<Button>(R.id.button)
+        var textView = findViewById<TextView>(R.id.textView)
 
         button.setOnClickListener(
             View.OnClickListener {
-                Toast.makeText(this, "CLicked On the Button", Toast.LENGTH_LONG).show()
-                val intent = Intent(this, MainActivity2::class.java)
-                startActivity(intent)
-                finish()
+//                Toast.makeText(this, "CLicked On the Button", Toast.LENGTH_LONG).show()
+//                val intent = Intent(this, MainActivity2::class.java)
+//                startActivity(intent)
+//                finish()
+                textView.setText("Hello Niko Bellic")
             })
 //        window.setFlags(
 //            WindowManager.LayoutParams.FLAG_FULLSCREEN,
